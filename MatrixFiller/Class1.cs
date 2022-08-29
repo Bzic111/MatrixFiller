0907533224eb8c1d15769254beed06448728f414
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-08-29 19:36:42 +0300

    Добавьте файлы проекта.

---
+++ MatrixFiller/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixFiller;
+
+public class Matrix
+{
+    int counter, leftBorder, rightBorder, upBorder, downBorder, lenghtVertical, lenghtHorizontal;
+    int[,] arr;
+    private void Init()
+    {
+        counter = 1;
+        leftBorder = rightBorder = upBorder = downBorder = 0;
+    }
+    public Matrix()
+    {
+        Init();
+    }
+    public void Selecting()
+    {
+        var key = Console.ReadKey(false);
+        switch (key.Key)
+        {
+
+            case ConsoleKey.UpArrow:
+                break;
+            case ConsoleKey.DownArrow:
+                break;
+            case ConsoleKey.Enter:
+                break;
+            case ConsoleKey.Escape:
+                break;
+            default:
+                break;
+        }
+    }
+    private void WriteArrayDU()
+    {
+        for (int i = lenghtVertical - downBorder; i >= 0 + upBorder; i--)
+            arr[i, rightBorder] = counter++;
+        rightBorder++;
+    }
+    private void WriteArrayRL()
+    {
+        for (int i = lenghtHorizontal - leftBorder; i >= 0 + rightBorder; i--)
+            arr[lenghtVertical - downBorder, i] = counter++;
+        downBorder++;
+    }
+    private void WriteArrayUD()
+    {
+        for (int i = 0 + upBorder; i <= lenghtVertical - downBorder; i++)
+            arr[i, lenghtHorizontal - leftBorder] = counter++;
+        leftBorder++;
+    }
+    private void WriteArrayLR()
+    {
+        for (int i = 0 + rightBorder; i <= lenghtHorizontal - leftBorder; i++)
+            arr[upBorder, i] = counter++;
+        upBorder++;
+    }
+    public void ArrayToConsole()
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+                Console.Write($"{arr[i, j]}\t");
+            Console.WriteLine();
+        }
+    }
+    public void FillMatrixRightDown()
+    {
+        while (counter < lenghtVertical * lenghtHorizontal)
+        {
+            WriteArrayLR();
+            WriteArrayUD();
+            WriteArrayRL();
+            WriteArrayDU();
+        }
+    }
+}
---
+++ MatrixFiller/Program.cs
@@ -0,0 +1,95 @@
+﻿Console.WriteLine("Input Height");
+int height = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Input Width");
+int width = int.Parse(Console.ReadLine()!);
+
+int counter = 1;
+int leftBorder, rightBorder, upBorder, downBorder, vertical, horizontal;
+leftBorder = rightBorder = upBorder = downBorder = 0;
+int[,] arr = new int[height, width];
+vertical = arr.GetLength(0) - 1;
+horizontal = arr.GetLength(1) - 1;
+
+FillMatrixRightDown(height, width, ref counter, ref leftBorder, ref rightBorder, ref upBorder, ref downBorder, vertical, horizontal, arr);
+
+ArrayToConsole(arr);
+
+static void WriteArrayDU(ref int counter,
+                         ref int rightBorder,
+                         int upBorder,
+                         int downBorder,
+                         int lenghtVertical,
+                         int[,] arr)
+{
+    for (int i = lenghtVertical - downBorder; i >= 0 + upBorder; i--)
+        arr[i, rightBorder] = counter++;
+    rightBorder++;
+}
+
+static void WriteArrayRL(ref int counter,
+                         int leftBorder,
+                         int rightBorder,
+                         ref int downBorder,
+                         int lenghtVertical,
+                         int lenghtHorizontal,
+                         int[,] arr)
+{
+    for (int i = lenghtHorizontal - leftBorder; i >= 0 + rightBorder; i--)
+        arr[lenghtVertical - downBorder, i] = counter++;
+    downBorder++;
+}
+
+static void WriteArrayUD(ref int counter,
+                         ref int leftBorder,
+                         int upBorder,
+                         int downBorder,
+                         int lenghtVertical,
+                         int lenghtHorizontal,
+                         int[,] arr)
+{
+    for (int i = 0 + upBorder; i <= lenghtVertical - downBorder; i++)
+        arr[i, lenghtHorizontal - leftBorder] = counter++;
+    leftBorder++;
+}
+
+static void WriteArrayLR(ref int counter,
+                         int leftBorder,
+                         int rightBorder,
+                         ref int upBorder,
+                         int lenghtHorizontal,
+                         int[,] arr)
+{
+    for (int i = 0 + rightBorder; i <= lenghtHorizontal - leftBorder; i++)
+        arr[upBorder, i] = counter++;
+    upBorder++;
+}
+
+static void ArrayToConsole(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+static void FillMatrixRightDown(int height,
+                                int width,
+                                ref int counter,
+                                ref int leftBorder,
+                                ref int rightBorder,
+                                ref int upBorder,
+                                ref int downBorder,
+                                int vertical,
+                                int horizontal,
+                                int[,] arr)
+{
+    while (counter < height * width)
+    {
+        WriteArrayLR(ref counter, leftBorder, rightBorder, ref upBorder, horizontal, arr);
+        WriteArrayUD(ref counter, ref leftBorder, upBorder, downBorder, vertical, horizontal, arr);
+        WriteArrayRL(ref counter, leftBorder, rightBorder, ref downBorder, vertical, horizontal, arr);
+        WriteArrayDU(ref counter, ref rightBorder, upBorder, downBorder, vertical, arr);
+    }
+}
